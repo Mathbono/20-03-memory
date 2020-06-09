9a854cff47410c5465f75eeccb2544983f9b6996
Mathieu Bonomini <bonomini.mathieu@yahoo.fr>
2020-06-09 16:00:28 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Memory
+
+## Jeu de mémorisation
+
+Trouvez la bonne paire d’images et tentez le meilleur score !
+
+## Lien
+
+[https://bonomini.website/memory/](https://bonomini.website/memory/)
---
+++ src/App.spec.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
+
+import App, { SYMBOLS } from './App';
+import GuessCount from './GuessCount';
+import Card from './Card';
+
+describe('<App />', () => {
+	it('renders without crashing', () => {
+		const wrapper = shallow(<App />);
+	})
+	it('contains a zero-guess counter', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper).to.contain(<GuessCount guesses={0} />);
+	})
+	it('has 36 cards', () => {
+		const wrapper = shallow(<App />);
+		expect(wrapper.find('Card')).to.have.length(36);
+	 })
+})
+describe('<Card/>', () => {
+	it('should trigger its `onClick` prop when clicked', () => {
+		const onClick = jest.fn();
+		const wrapper = shallow(
+			<Card card="😁" feedback="hidden" index={0} onClick={onClick} />
+		);
+		wrapper.simulate('click');
+		expect(onClick).toHaveBeenCalledWith(0);
+	})
+	it('should trigger its `onClick` prop when clicked', () => {
+		const onClick = sinon.spy();
+		const wrapper = shallow(
+			<Card card="😁" feedback="hidden" index={0} onClick={onClick} />
+		);
+		wrapper.simulate('click');
+		expect(onClick).to.have.been.calledWith(0);
+	})
+	it('should match its reference snapshot', () => {
+		const onClick = sinon.spy();
+		const wrapper = shallow(
+			<Card card="😁" feedback="hidden" index={0} onClick={onClick} />
+		);
+		expect(wrapper).to.matchSnapshot();
+	})
+	it('should match its reference snapshot', () => {
+		const mock = sinon
+			.stub(App.prototype, 'generateCards')
+			.returns([...SYMBOLS.repeat(2)]);
+		try {
+			const wrapper = shallow(<App />);
+			expect(wrapper).to.matchSnapshot();
+		} finally {
+			mock.restore();
+		}
+	})
+})
